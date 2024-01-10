f2a06f35e0c14e6abcd03ce86c7105876c146ba6
CamCrocs <crockettcameron6@gmail.com>
2024-01-10 14:08:05 -0500

    Initial commit for mobile dev

---
+++ SpotifyApp/SpotifyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyApp
+//
+//  Created by Fletcher Crockett on 1/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
